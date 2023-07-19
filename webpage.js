e02382ccd63310a95cfacf62e75c0e3bc24167f2
Train16 <train16@AG.US.SWRI.ORG>
2023-07-19 14:15:49 -0500

    add files

---
+++ webpage.js
@@ -0,0 +1,9 @@
+var data = [
+  {
+    x: ['Blue', 'Cyan', 'Green', 'Yellow', 'Orange', 'Red', 'Deep-Red', 'IR1', 'IR2', 'IR3', 'IR4'],
+    y: [24.56, 17.27, 11.19, 10.54, 10.34, 18.74, 22.01, 12.86, 65.64, 95.18, 98.16],
+    type: 'bar'
+  }
+];
+
+Plotly.newPlot('myDiv', data);
